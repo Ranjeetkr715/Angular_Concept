dad791cf8585fdb478cc644e3af78eef5bb53bfa
ranjeetkumar <ranjeetkr369@gmail.com>
2025-04-27 15:33:18 +0530

    Angular Data type in angular

--- angular-concept-tut/src/app/app.component.ts
+++ angular-concept-tut/src/app/app.component.ts
@@ -11,9 +11,26 @@ import { SignupComponent } from './signup/signup.component';
 })
 export class AppComponent {
   title = 'angular-concept-tut';
-  name = 'Ranjeet Kumar';
+  name:string = 'Ranjeet';
+  data: string|number ="hello";
+  other : any = true;
   x =10;
   y=20;
   user1 = 'Ranjeet';
   user2 = "Ranjeet";
+
+  updateUsername(){
+    let x = 30;
+    this.name='Ranjeet Kumar';
+    this.data=4321;
+    this.other="abcd";
+    console.log(x);
+    console.log(this.name);
+    console.log(this.data);
+    console.log(this.other);
+    }
+
+  sum (a:number,b:number){
+        console.log(a+b);
+    }
 }
